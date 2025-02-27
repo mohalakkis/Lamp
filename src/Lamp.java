64d290c0a2550f25884628ab497374532fba6136
mohamed.lakkis <mohamed.lakkis@consultores.rnp.br>
2025-02-27 11:26:24 -0300

    feat: cria uma lampada com seus methodos

---
+++ src/Lamp.java
@@ -0,0 +1,22 @@
+public class Lamp {
+    private boolean on;
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void turnOn() {
+        setOn(true);
+    }
+
+    public void turnOff() {
+        setOn(false);
+    }
+    public boolean getOn() {
+        return on;
+    }
+
+    public void showState() {
+        System.out.println("Lâmpada está " + (getOn() ? "ligada" : "desligada"));
+    }
+}
